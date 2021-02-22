44db7b6668640761889cb0c7b2c8702643f0c7dc
Rossan P <53863541+rossanp@users.noreply.github.com>
2021-02-22 11:30:55 -0300

    Render header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,28 @@
 import React from "react"
-import Styled from "styled-components"
+/* import Styled from "styled-components" */
 
+/* const logo = <div></div>
+const menu = (
+    <div>
+
+    </div>
+) */
+
+export default class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    <p>LOGO</p>
+                </div>
+                <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Cardápio</li>
+                    <li>Sobre</li>
+                </ul>
+                </div>
+            </div>
+        )
+    }
+}
